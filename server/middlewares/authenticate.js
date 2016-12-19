27eb07f0dea87091843c77be094a76db944e688d
bhumika <bhumika@improwised.com>
2016-12-19 15:51:57 +0530

    fix bug in authenticate middleware

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -21,10 +21,10 @@ export default (req, res, next) => {
         }).fetch().then(user => {
           if (!user) {
             res.status(404).json({ errors: 'No such User'});
+          } else {
+            req.currentUser = user;
+            next();
           }
-
-          req.currentUser = user;
-          next();
         });
       }
     });
