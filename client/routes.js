9cb3079a8ccebdaa9c2d276bec9475e340daf5a0
bhumika <bhumika@improwised.com>
2016-12-19 17:49:15 +0530

    requireAuth higher order component

--- client/routes.js
+++ client/routes.js
@@ -7,12 +7,14 @@ import SignupPage from './components/signup/SignupPage';
 import LoginPage from './components/login/LoginPage';
 import newEventPage from './components/events/newEventPage';
 
+import requireAuth from './utils/requireAuth';
+
 export default (
   <Route path="/" component={App} >
     <IndexRoute component={Greetings} />
     <Route path="signup" component={SignupPage} />
     <Route path="login" component={LoginPage} />
-    <Route path="new-event" component={newEventPage} />
+    <Route path="new-event" component={requireAuth(newEventPage)} />
   </Route>
 )
 
